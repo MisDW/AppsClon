b8386d6343b37465fe590e8a19b7cf72660c71bd
MisDW <96786803+MisDW@users.noreply.github.com>
2024-02-09 04:04:19 -0600

    Add files via upload

---
+++ open_img.js
@@ -0,0 +1,6 @@
+const nombre_archivo = document.querySelector(".nombre_archivo")
+nombre_archivo.addEventListener("click", () =>{
+    console.warn("open file...");
+    window.location = "img.html"
+    
+})
